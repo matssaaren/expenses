c641557b4d4fb188449aa0ad039668922d5eaeb8
Mats Saaren <mats.saaren@voco.ee>
2025-03-24 12:03:45 +0200

    Poolik 7. Komponentide kompositsioon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Expenses from './components/Expenses'
 
 function App() {
-
+  // Pooleli 7. Komponentide kompositsioon ja failis Card.js
   const expenses = [
     {
       date: new Date(2025, 10, 12),
