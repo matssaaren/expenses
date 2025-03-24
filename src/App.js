1de6662da1ed70518cfc3382439106f71bb49c9e
Mats Saaren <mats.saaren@voco.ee>
2025-03-24 11:29:38 +0200

    3. Komponendi sisustamine andmetega

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenses = [
+    {
+      date: new Date(2025, 10, 12),
+      title: 'New Book',
+      price: 30.99
+    },
+    {
+      date: new Date(2025, 10, 12),
+      title: 'New Jeans',
+      price: 99.99
+    }
+  ]
+  
+
   return (
     <div className="App">
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   );
 }
