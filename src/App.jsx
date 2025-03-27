2760980c763a1b120ca5a4d41edb70c44073397b
Mats Saaren <mats.saaren@voco.ee>
2025-03-27 16:47:13 +0200

    18. LocalStorage  ja useEffect hooki kasuta...

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'id1',
-    date: new Date(2024, 10, 12),
-    title: 'New Book',
-    amount: 30.99
-  },
-  {
-    id: 'id2',
-    date: new Date(2025, 9, 12),
-    title: 'New Jeans',
-    amount: 99.99
-  },
-  {
-    id: 'id3',
-    date: new Date(2025, 8, 12),
-    title: 'New Bag',
-    amount: 199.99
-  }
-]
+// const DUMMY_EXPENSES = [
+//   {
+//     id: 'id1',
+//     date: new Date(2024, 10, 12),
+//     title: 'New Book',
+//     amount: 30.99
+//   },
+//   {
+//     id: 'id2',
+//     date: new Date(2025, 9, 12),
+//     title: 'New Jeans',
+//     amount: 99.99
+//   },
+//   {
+//     id: 'id3',
+//     date: new Date(2025, 8, 12),
+//     title: 'New Bag',
+//     amount: 199.99
+//   }
+// ]
 
 function App() {
 
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+  const [expenses, setExpenses] = useState(() => {
+    const expensesFormLS = JSON.parse(localStorage.getItem('expenses'))
+    return expensesFormLS || []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('expenses', JSON.stringify(expenses))
+  }, [expenses])
 
   const addExpenseHandler = (expense) => {
     setExpenses((previousExpenses) => {
