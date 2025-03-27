aa0533a7946aaf67028db2b3f65321edf56f5246
Mats Saaren <mats.saaren@voco.ee>
2025-03-27 15:51:37 +0200

    16. Ülesanne 6 - valikulise kuvamise

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -12,7 +12,7 @@ const ExpenseForm = (props) => {
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: enteredDate
+            date: new Date(enteredDate)
         }
         props.onSaveExpenseData(expenseData)
         setEnteredTitle('')
@@ -52,6 +52,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,18 +1,35 @@
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm'
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+import React, {useState} from 'react';
 
 const NewExpense = (props) => {
-    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+      setIsEditing(true);
+      console.log('form edit open')
+  }
+
+  const stopEditingHandler = () => {
+      setIsEditing(false);
+      console.log('form edit close')
+  }
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData)
+        stopEditingHandler();
     }
-    return(
-        <div className='new-expense'>
-           <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
-        </div>
-    )
+
+  return (
+    <div className="new-expense">
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
+    </div>
+  );
 }
-export default NewExpense
+
+export default NewExpense;
